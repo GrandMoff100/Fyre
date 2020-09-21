15a69cf56eca178571c5c8be0c61efcc4bb13c80
GrandMoff100 <minecraftcrusher100@gmail.com>
2020-09-21 15:12:56 -0500

    Added base packages and example script

---
+++ FyreScript/Tokenizer/tokenize.py
@@ -0,0 +1,8 @@
+class Tokenizer:
+    def __init__(self):
+        pass
+
+
+class Token:
+    def __init__(self):
+        pass
---
+++ example.py
@@ -0,0 +1,2 @@
+with open("example.fys", 'r') as fys:
+    lines = fys.readlines()
