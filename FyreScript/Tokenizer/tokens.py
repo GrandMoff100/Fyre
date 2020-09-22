aff6302e3c6cf008a51de3f82d2b6072421b60cc
Wizard Hat <Nlarsen23.student@gmail.com>
2020-09-22 15:39:24 +0000

    simplified lexers

--- FyreScript/Tokenizer/tokens.py
+++ FyreScript/Tokenizer/tokens.py
@@ -12,29 +12,3 @@ KEYWORDS = [
 class Token:
     def __init__(self, text, line_num):
         pass
-
-    @staticmethod
-    def _is_int(text):
-        return text.isnumeric()
-
-    @staticmethod
-    def _is_float(text):
-        if '.' in text:
-            if text.replace('.', '', 1).isdecimal():
-                return True
-        return False
-
-    @staticmethod
-    def _is_string(text):
-        for char in STRING_INDICATORS:
-            if text.startswith(char) and text.endswith(char):
-                return True
-        return False
-
-    @staticmethod
-    def _is_list(text):
-        pass
-
-    @staticmethod
-    def _is_dict(text):
-        pass
